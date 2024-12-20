507865dae532f0ac5f4d898bedcfe95d765abc33
Hecoal <hectorcortez_2002@hotmail.com>
2024-12-20 13:14:36 -0600

    Added seeding to database

--- DiaryApp/Data/ApplicationDbContext.cs
+++ DiaryApp/Data/ApplicationDbContext.cs
@@ -10,5 +10,32 @@ namespace DiaryApp.Data
 
         }
         public DbSet<DiaryEntry>DiaryEntries{get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<DiaryEntry>().HasData(
+                    new DiaryEntry { 
+                        Id= 1, 
+                        Title= "Went to the cinema", 
+                        Content= "Went to the cinema to watch a movie.", 
+                        Created=DateTime.Today
+                    },
+                    new DiaryEntry
+                    {
+                        Id = 2,
+                        Title = "Went shopping",
+                        Content = "Went shopping unicorn onesies.",
+                        Created = DateTime.Today
+                    },
+                    new DiaryEntry
+                    {
+                        Id = 3,
+                        Title = "Went diving",
+                        Content = "Went diving after shopping!",
+                        Created = DateTime.Today
+                    }
+                );
+        }
     }
 }
